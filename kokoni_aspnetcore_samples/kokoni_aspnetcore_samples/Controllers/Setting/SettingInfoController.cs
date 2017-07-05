771a9a5e2d802076a78568db01b48a52054b8d22
kinoshita <kingkino@gmail.com>
2017-07-05 11:53:29 +0900

    Landingページのデザインを修正

---
+++ kokoni_aspnetcore_samples/kokoni_aspnetcore_samples/Controllers/Setting/SettingInfoController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace kokoni_aspnetcore_samples.Controllers.Setting
+{
+    public class SettingInfoController : Controller
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public SettingInfoController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            string settings = string.Empty;
+
+            settings += _hostingEnvironment.ApplicationName + Environment.NewLine;
+            settings += _hostingEnvironment.EnvironmentName + Environment.NewLine;
+
+            settings += "---------------------------------------------------------------------------------------------" + Environment.NewLine;
+
+            settings += _hostingEnvironment.WebRootPath + Environment.NewLine;
+            settings += _hostingEnvironment.ContentRootPath + Environment.NewLine;
+            settings += _hostingEnvironment.WebRootFileProvider + Environment.NewLine;
+            settings += _hostingEnvironment.ContentRootFileProvider + Environment.NewLine;
+
+            ViewData["setting"] = settings;
+
+            return View();
+        }
+    }
+}
---
+++ kokoni_aspnetcore_samples/kokoni_aspnetcore_samples/Controllers/Tutorial1/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace kokoni_aspnetcore_samples.Controllers.Tutorial1
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        [Route("Tutoral1")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /HelloWorld/Welcome/ 
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
